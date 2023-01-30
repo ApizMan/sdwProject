67580d75dd14afdb5e1bf531170a2c4ef24ef3a1
Nafiz Mansor <nafizmansor@gmail.com>
2023-01-30 12:05:40 +0800

    no message

--- lib/screen/alumni/donation_alumni/components/donation_paymant/alumni_letter.dart
+++ lib/screen/alumni/donation_alumni/components/donation_paymant/alumni_letter.dart
@@ -16,61 +16,74 @@ class _AlumniLetterState extends State<AlumniLetter> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 100),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 50),
-              child: Container(
-                child: IconButton(
-                  onPressed: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            NavigationAlumni(selectNavDefault: 2),
-                      ),
-                    );
-                  },
-                  icon: Icon(Icons.close),
-                ),
-                width: 50.0,
-                height: 50.0,
-                decoration: BoxDecoration(
-                  color: Colors.transparent,
-                  borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 2.0,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 100),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 50),
+                child: Container(
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              NavigationAlumni(selectNavDefault: 2),
+                        ),
+                      );
+                    },
+                    icon: Icon(Icons.close),
+                  ),
+                  width: 50.0,
+                  height: 50.0,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 2.0,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Center(
-              child: Column(children: [
-                Container(
-                  height: 400,
-                  width: 400,
-                  child: Image(
-                    image: AssetImage('assets/images/imgReceipt.png'),
-                    fit: BoxFit.fill,
+              Center(
+                child: Column(children: [
+                  Container(
+                    height: 400,
+                    width: 400,
+                    child: Image(
+                      image: AssetImage('assets/images/imgReceipt.png'),
+                      fit: BoxFit.fill,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  height: 200,
-                  width: 250,
-                  child: Text(
-                      'Thank you for your donation. May your deeds be accepted as good deed’s and meaningful to others',
-                      style: TextStyle(fontSize: 22)),
-                )
-              ]),
-            ),
-          ],
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    height: 140,
+                    width: 250,
+                    child: Text(
+                        'Thank you for your donation. May your deeds be accepted as good deed’s and meaningful to others',
+                        style: TextStyle(fontSize: 22)),
+                  ),
+                  Container(
+                    width: 250,
+                    child: Text(
+                      'Get your certificate here',
+                      style: TextStyle(
+                        fontSize: 12,
+                        decoration: TextDecoration.underline,
+                        color: Color.fromARGB(255, 15, 92, 155),
+                      ),
+                    ),
+                  )
+                ]),
+              ),
+            ],
+          ),
         ),
       ),
     );
